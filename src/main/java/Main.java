f69c7a0107c933eed10c68d79b2b46d5e151ceb1
Nusera <nusera@zipcoders-MBP.fios-router.home>
2022-03-01 21:32:11 -0500

    greeted alice and bob

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args ){
+    public static void main(String[] args) {
+        System.out.println("What is your name?");
+        Scanner userInput = new Scanner(System.in);
+        String name = userInput.nextLine();
+        if (name.equals("Alice")) {
+            System.out.println("Hello, Alice.");
+        } else if (name.equals("Bob")) {
+            System.out.print("Hello, Bob.");
+        } else {
+            System.out.println("You are not ALice or Bob.");
+        }
 
     }
 }
